3ea326e09a73737b4435b08ba7891164531b7a8d
PrinceBG <abouefibele@gmail.com>
2021-12-09 20:33:26 +0100

    modifications pages et ajout

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,51 @@
 /* ============= Header Section ================ */
 
-const contain = document.querySelector('.heading');
+// ================= Navigation Focus a ==========
+
+const links = document.querySelectorAll('.nav-list>a')
+console.log(links)
+let linkcomteur = 0;
+
+const removeContent_a = () =>{
+    for( let i = 0; i < links.length; i++){
+        links[i].classList.remove('active-a')
+    }  
+}
+
+links.forEach((item) => {
+    item.addEventListener('click', () => ajoutBottom(item))
+});
+
+const ajoutBottom = (element) => {
+    removeContent_a();
+    element.classList.add('active-a');
+   } ;
+
+
+
+
+// =========== Body Mode nuit =============== 
+
+const moon  = document.querySelector('.moon');
+const Header = document.querySelector('.heading');
+
+moon.onclick = () => {
+    moon.classList.toggle('fa-moon');
+   
+    
+   if(moon.classList.contains('fa-moon')){
+        document.body.classList.add('activate');
+        Header.style.background = '#020207';
+    }
+    else{
+        document.body.classList.remove('activate'); 
+    }
+}
+
+
+// =========== Navigation Menu ===============
+   
+
 
 const nav = document.querySelector('.nav');
 
@@ -25,7 +70,6 @@ const closeMenu = () => {
 const container = document.querySelector('.box');
 
 const slide = document.querySelectorAll('.content');
-console.log(slide)
 const sildelenght = slide.length;
 
 const removeContent = () =>{
@@ -41,8 +85,6 @@ let activeindex = 0;
 rigth.addEventListener('click', () => changeSlide('up'));
 left.addEventListener('click', () => changeSlide('down'));
 
-
- console.log(slide)
 
 const changeSlide = (direction) => {
     removeContent()
@@ -71,3 +113,45 @@ setInterval(() => {
     removeContent()
     slide[activeindex].classList.add('active')
 }, 5000);
+
+// Section Collaboration 
+
+const colab = document.querySelector('.box-colb-img');
+const suivant = document.querySelector('.arrow-right');
+const precedent = document.querySelector('.arrow-left');
+
+suivant.addEventListener('click', () => ImgSuivant('up'));
+precedent.addEventListener('click', () => ImgSuivant('down'));
+
+let compteur = 0;
+
+console.log(colab);
+
+// Affichage élements au scroll
+const ratio = .1
+const options = {
+    root:null,
+    rootMargin: '0px',
+    threshold: ratio
+  }
+  
+  const handelIntersect = (entries, observe) =>{
+      entries.forEach((entry) =>{
+        if(entry.intersectionRatio > ratio){
+            entry.target.classList.add('reavel-visible')
+            console.log('visible')
+            // observer.unobserve(entry.target)
+        }
+        else if(entry.intersectionRatio == -.4){
+            entry.target.classList.remove('reavel-visible')   
+        }
+        else{
+            entry.target.classList.remove('reavel-visible')
+        } 
+      })
+  }
+  var observer = new IntersectionObserver(handelIntersect, options);
+
+ document.querySelectorAll('.reavel').forEach((reavel) =>{
+    observer.observe(reavel);
+ })
